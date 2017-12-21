85e010fe5de050a083bec41fa9f916a9f3d89739
Hiroyoshi Noro <norotime@users.noreply.github.com>
2017-12-21 11:19:10 +0900

    Refactoring

--- encapsulation/ruby/main.rb
+++ encapsulation/ruby/main.rb
@@ -1,18 +1,13 @@
-class Main
-  require './bad_car'
-  require './car'
-  require './execute'
-	
-  def main
-    exe = Execute.new
-    # 良い例
-    puts "-- 良い例 -- "
-    exe.excute_car
-    # 悪い例
-    puts "-- 悪い例 -- "
-    exe.excute_bad_car
-  end
+require './bad_car'
+require './car'
+require './execute'
 
+if __FILE__ == $0
+  exe = Execute.new
+  # 良い例
+  puts "-- 良い例 -- "
+  exe.excute_car
+  # 悪い例
+  puts "-- 悪い例 -- "
+  exe.excute_bad_car
 end
-
-Main.new.main
