dab773e488f904773bba39578f2b3021f845df6b
Hiroyoshi Noro <norotime@users.noreply.github.com>
2017-12-21 13:51:14 +0900

    Refactoring

--- inheritance/ruby/soccer_man.rb
+++ inheritance/ruby/soccer_man.rb
@@ -2,53 +2,53 @@ class SoccerMan
 
   # スタミナ、スピード、スキル
   def initialize
-	  @stamina = 100
-		@pace = 0
-		@skill = 0
+    @stamina = 100
+    @pace = 0
+    @skill = 0
   end
 
   def set_skill(new_skill)
     @skill = new_skill
   end
 
-	def get_status
+  def get_status
     puts("stamina:" + @stamina.to_s)
-		puts("pace:" + @pace.to_s)
-		puts("skill:" + @skill.to_s)
+    puts("pace:" + @pace.to_s)
+    puts("skill:" + @skill.to_s)
   end
 
-	def run
+  def run
     puts " -- run -- "
     @stamina -= 1
     @pace += 1
   end
 
-	def stop
+  def stop
     puts " -- stop -- "
     @pace -= 1
   end
 
-	def rest
+  def rest
     puts " -- rest -- "
     @stamina = 100
   end
 
-	def acceleration
+  def acceleration
     puts " -- acceleration -- "
     @stamina -= 1
-		@stamina -= 1
-		@stamina -= 1
-		@pace += 5
+    @stamina -= 1
+    @stamina -= 1
+    @pace += 5
   end
 
   def sprint_speed
     puts " -- sprint_speed -- "
     @stamina -= 1
-		@stamina -= 1
-		@pace += @skill
-	end
+    @stamina -= 1
+    @pace += @skill
+  end
 
-	def practice
+  def practice
     puts " -- practice -- "
     @skill += @skill
   end
