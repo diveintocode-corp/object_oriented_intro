569d636184611e5e249751ebaa175de9086b9da0
Hiroyoshi Noro <norotime@users.noreply.github.com>
2017-12-21 13:50:09 +0900

    Refactoring

--- inheritance/ruby/execute.rb
+++ inheritance/ruby/execute.rb
@@ -7,16 +7,16 @@ class Execute
     super_car.get_status
     super_car.stop
     super_car.get_status
-	end
+  end
 
-	def excute_truck
-		truck = Truck.new
-		truck.get_status
-		truck.run
-		truck.load
-		truck.get_status
-		truck.stop
-		truck.get_status
-	end
+  def excute_truck
+    truck = Truck.new
+    truck.get_status
+    truck.run
+    truck.load
+    truck.get_status
+    truck.stop
+    truck.get_status
+  end
 
 end
